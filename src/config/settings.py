30f7f81198ce7f304ccd9bcc8f99c6ed878c5189
berdibaevich <berdibaevich.me@gmail.com>
2026-05-04 00:09:55 +0500

    Feat(django): Add new User app

--- src/config/settings.py
+++ src/config/settings.py
@@ -21,6 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project's Apps
+    "apps.user.apps.UserConfig",
+    
 ]
 
 MIDDLEWARE = [
