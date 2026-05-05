72e8682b7428e379b4393f0efebf5a28d8cfb428
berdibaevich <berdibaevich.me@gmail.com>
2026-05-05 10:48:53 +0500

    feat: create telegramGroup model

--- src/apps/students/models.py
+++ src/apps/students/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TelegramGroup(models.Model):
+    class DayType(models.TextChoices):
+        ODD_DAYS = 'ODD', 'Odd Days (Mon/Wed/Fri)' 
+        EVEN_DAYS = 'EVEN', 'Even Days (Tue/Thu/Sat)'
+
+    class TimeSlot(models.TextChoices):
+        AFTERNOON_1 = '14:00', '14:00'
+        AFTERNOON_2 = '16:00', '16:00'
+    
+
+    days = models.CharField(max_length=4, choices=DayType.choices)
+    time = models.CharField(max_length=5, choices=TimeSlot.choices)
+
+    telegram_group_id = models.BigIntegerField(
+        _("Telegram Group ID"),
+        unique=True
+    )
+
+    # Status
+    is_active = models.BooleanField(_("Is Active"), default=True)
+    is_full = models.BooleanField(_("Is Full"), default=False, editable=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'telegram_group'
+        verbose_name = 'Telegram group'
+        verbose_name_plural = 'Telegram groups'
+
+
+    def __str__(self):
+        return f"{self.days} ({self.time})"
+
