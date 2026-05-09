2acc30975cd32977a7cb91521e8058cab982bca3
berdibaevich <berdibaevich.me@gmail.com>
2026-05-10 02:40:07 +0500

    feat: update start command

--- src/apps/bot/handlers/user.py
+++ src/apps/bot/handlers/user.py
@@ -2,9 +2,25 @@ from aiogram import Router
 from aiogram.filters.command import Command
 from aiogram.types import Message
 
+from ..keyboards import (
+    get_role_ik
+)
+
+from apps.students.services import has_active_groups_exists
+
 
 user_private_router = Router()
 
 @user_private_router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Hello Bro.")
+    has_groups = await has_active_groups_exists()
+    
+    if has_groups:
+        text = (
+            "<b>Assalawma aleykum!</b>\n\n"
+            "Baqla botqa xosh keldińiz. ✨"
+            "\nTanlań:"
+        )
+        await message.answer(text, reply_markup=get_role_ik(), parse_mode="HTML")
+    else:
+        await message.answer("Hello.")
