a8b5f8840c318d000b77e114c28709d4a9aeb7a0
berdibaevich <berdibaevich.me@gmail.com>
2026-05-10 03:46:31 +0500

    feat: add redis configuration for FSM storage

--- src/apps/bot/loader.py
+++ src/apps/bot/loader.py
@@ -1,8 +1,15 @@
 from django.conf import settings
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+
+from utils.aredis_service import get_async_redis_client
+
+
+storage = RedisStorage(redis=get_async_redis_client(6))
+
 
 bot = Bot(token=settings.BOT_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
 
 
 def setup_routers(dp: Dispatcher):
