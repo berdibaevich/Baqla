e10ed5dd5d1ad7b577bc5ca17ac4e0db998f76a4
berdibaevich <berdibaevich.me@gmail.com>
2026-05-10 14:46:19 +0500

    feat: add select groups inline keyboard func

--- src/apps/bot/keyboards/__init__.py
+++ src/apps/bot/keyboards/__init__.py
@@ -1,7 +1,9 @@
 from .inline import (
-    get_role_ik
+    get_role_ik,
+    get_select_groups_ik
 )
 
 __all__ = (
     "get_role_ik",
+    "get_select_groups_ik"
 )
--- src/apps/bot/keyboards/inline.py
+++ src/apps/bot/keyboards/inline.py
@@ -3,8 +3,11 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from ..callback import (
     UserAction,
-    RoleCbData
+    RoleCbData,
+    GroupCbData
 )
+
+from utils import format_group_name
 
 
 def get_role_ik() -> InlineKeyboardMarkup:
@@ -22,3 +25,20 @@ def get_role_ik() -> InlineKeyboardMarkup:
     )
     builder.adjust(2)
     return builder.as_markup()
+
+
+
+def get_select_groups_ik(list_groups: list) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+
+    for group in list_groups:
+        text = format_group_name(group['days'], group['time'])
+
+        builder.button(
+            text=text,
+            callback_data=GroupCbData(action=UserAction.SELECT, id=group['id'])
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
+
