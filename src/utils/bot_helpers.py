4cdc6f227910b1f32855038e164ddea0546ed1eb
berdibaevich <berdibaevich.me@gmail.com>
2026-05-11 00:59:33 +0500

    feat: Create delete old messages func

--- src/utils/bot_helpers.py
+++ src/utils/bot_helpers.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from aiogram import Bot
+from aiogram.exceptions import TelegramBadRequest
+
 
 def format_group_name(days: str, time: str) -> str:
     translations = {
@@ -6,3 +10,23 @@ def format_group_name(days: str, time: str) -> str:
     }
     day_name = translations.get(days.upper(), days)
     return f"{day_name} | 🕒 {time}"
+
+
+
+async def delete_old_messages(bot: Bot, chat_id: int, message_ids: list[int]):
+    """
+        Delete old messages from spesific user's chat by IDs.
+    """
+    for msg_id in message_ids:
+        try:
+            await bot.delete_message(chat_id=chat_id, message_id=msg_id)
+        except TelegramBadRequest as e:
+            await bot.send_message(
+                chat_id=settings.SUPERUSER_TELEGRAM_ID,
+                text=f"Xabardi óshiriwde qátelik (ID: {msg_id}): \n\n{e}"
+            )
+        except Exception as e:
+            await bot.send_message(
+                chat_id=settings.SUPERUSER_TELEGRAM_ID,
+                text=f"Kútilmegen qátelik: {e}"
+            )
