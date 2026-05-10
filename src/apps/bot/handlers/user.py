d2ab9de6a97614aee0ee640c5bfe97d531c55e3a
berdibaevich <berdibaevich.me@gmail.com>
2026-05-11 01:00:46 +0500

    feat: update start cmd

--- src/apps/bot/handlers/user.py
+++ src/apps/bot/handlers/user.py
@@ -6,14 +6,20 @@ from aiogram.types import Message
 from ..keyboards import (
     get_student_reg_ik
 )
+from utils.bot_helpers import delete_old_messages
+
 
 from apps.students.services import has_active_groups_exists
 
 
 user_private_router = Router()
 
+
 @user_private_router.message(Command("start"))
 async def cmd_start(message: Message, state: FSMContext):
+    data = await state.get_data()
+    message_ids = data.get("message_ids", [])
+    await delete_old_messages(bot=message.bot, chat_id=message.chat.id, message_ids=message_ids)
     await state.clear()
 
     has_groups = await has_active_groups_exists()
