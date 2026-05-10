24fad31bfc6f3f13a97baa2e0b3defb257ab03f1
berdibaevich <berdibaevich.me@gmail.com>
2026-05-10 14:40:01 +0500

    feat: add get_groups func for student and parent registration

--- src/apps/students/services.py
+++ src/apps/students/services.py
@@ -4,3 +4,7 @@ from .models import TelegramGroup
 async def has_active_groups_exists() -> bool:
     exists = await TelegramGroup.objects.filter(is_active_group=False).aexists()
     return exists
+
+
+async def get_groups():
+    return [group async for group in TelegramGroup.objects.filter(is_active_group=False, is_full=False).values("id", "days", "time")]
