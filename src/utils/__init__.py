8a444a9b972b79e7ce08c036a2aa56b21d31db4b
berdibaevich <berdibaevich.me@gmail.com>
2026-05-16 22:52:47 +0500

    refactor: remove is_valid_full_name import path

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,9 @@
 from .bot_helpers import (
     format_group_name,
     delete_old_messages,
-    is_valid_full_name
 )
 
 __all__ = (
     "format_group_name",
     "delete_old_messages",
-    "is_valid_full_name"
 )
